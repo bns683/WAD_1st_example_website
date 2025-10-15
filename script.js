b3eb667bfa69e821cb2e7d434d98a72974bd4559
KateHan <k.han4@edu.salford.ac.uk>
2025-10-15 11:38:42 +0100

    Add sendEmail function for email submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function sendEmail(event) {
+  event.preventDefault();
+  
+  const name = document.getElementById("name").value;
+  const message = document.getElementById("message").value;
+  const email = document.getElementById("email").value;
 
+  const subject = encodeURIComponent("Restaurant Booking / Feedback");
+  const body = encodeURIComponent(`Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`);
+  const mailtoLink = `mailto:info@doerestaurant.com?subject=${subject}&body=${body}`;
+  
+  window.location.href = mailtoLink;
+}
